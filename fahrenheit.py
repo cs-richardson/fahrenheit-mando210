7c0f13e01da13f8221819b5aff08a2661e0b34bd
Miki Ando <MikiAndo@Mikis-MacBook-Pro.local>
2019-08-16 09:21:11 +0700

    Fahrenheit Assignment

---
+++ fahrenheit.py
@@ -0,0 +1,8 @@
+# This program converts the temperature that the user inputs in ˚C to ˚F
+# Miki Ando
+c=input("What's the Temperature in ˚C? ")
+print("C: "+c)
+celsius= float(c)
+f= (1.8*celsius) + 32
+fahrenheit= str(f)
+print("F: " +fahrenheit)
